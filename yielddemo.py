70ef65460163a0d088854af431500fcf8eee13ef
BBOOT <wwwang9001@163.com>
2016-11-15 20:45:55 +0800

    Update yielddemo.py
    添加使用python自己的属性读取大文件

--- yielddemo.py
+++ yielddemo.py
@@ -13,8 +13,17 @@ def read_in_block(filename):
 			else:
 				return
 
-
+def read_inline(filename):
+	"""使用python自带的方法读取大文件
+	"""
+	with open(filename,"r") as fd:
+		for line in fd:
+			print block
+			
+			
 if __name__ == "__main__":
 	filename = sys.argv[1]
 	for block in read_in_block(filename):
 		print block
+		
+	read_inline(filename)

4e500226d6822bc79711f92d706ec09406fc26b1
bboot <bboot9001>
2016-11-15 23:10:07 +0800

    修改重复输出的问题

--- yielddemo.py
+++ yielddemo.py
@@ -6,24 +6,24 @@ def read_in_block(filename):
 	"""
 	BLOCK_SIZE = 8096
 	with open(filename,"r") as fd:
-		while True:
+		block = fd.read(BLOCK_SIZE)
+		while block:
+			yield block
 			block = fd.read(BLOCK_SIZE)
-			if block:
-				yield block
-			else:
-				return
+#			yield block
 
 def read_inline(filename):
 	"""使用python自带的方法读取大文件
 	"""
 	with open(filename,"r") as fd:
 		for line in fd:
-			print block
+			print line
 			
 			
 if __name__ == "__main__":
 	filename = sys.argv[1]
 	for block in read_in_block(filename):
 		print block
-		
-	read_inline(filename)
+#	read_in_block(filename)
+	print "*********开始第二个输出*********"	
+#	read_inline(filename)

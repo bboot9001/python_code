a8f1a2205ce0d7fe29102d303b29cd95edbb9642
bboot9001 <wwwang9001@163.com>
2016-12-28 23:55:25 +0800

    修改装饰器函数

--- decorate_learn.py
+++ decorate_learn.py
@@ -6,7 +6,21 @@
 def myfunc():
     print "myfunc() called."
 
+""" 示例二 使用函数在函数的执行前后分别附加额外功能"""
+
+def deco(func):
+    print "before myfunc called."
+    func()
+    print "after myfunc called."
+    return func
+
+def myfunc2():
+    print "myfunc called."
+
+myfunc2 = deco(myfunc2)
 
 if __name__ == '__main__':
-    myfunc()
-    myfunc()
+#    myfunc()
+#    myfunc()
+     myfunc2()
+     myfunc2()
